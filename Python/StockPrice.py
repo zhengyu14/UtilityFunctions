e98f33aed32f584b396257eff7c633675350312a
Zhengyu Zhu <zhengyu.a.zhu@gmail.com>
2020-09-01 11:46:42 +0800

    20200831

---
+++ Python/StockPrice.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+from pandas_datareader import data
+from datetime import datetime, timedelta
+
+
+def get_price_data(symbols, start_date, end_date):
+    return data.DataReader(symbols, 'yahoo', start_date, end_date)
+    
+def get_close_price_1w(symbols):
+    end_date=datetime.today()
+    start_date=datetime.today()-timedelta(days=7)
+    return get_price_data(symbols, start_date, end_date)
+
+def get_close_price_1m(symbols):
+    end_date=datetime.today()
+    start_date=datetime.today()-timedelta(days=30)
+    return get_price_data(symbols, start_date, end_date)
+
+def get_close_price_1y(symbols):
+    end_date=datetime.today()
+    start_date=datetime.today()-timedelta(days=365)
+    return get_price_data(symbols, start_date, end_date)
+
+get_close_price_1m('1810.hk').Close.plot()
